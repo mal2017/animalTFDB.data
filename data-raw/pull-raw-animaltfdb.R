1f9c4638010b4a2174c96d2f65b5d34c676d3f5f
mal2017 <matt.a.lawlor@gmail.com>
2018-04-23 18:18:30 -0400

    new data

--- data-raw/pull-raw-animaltfdb.R
+++ data-raw/pull-raw-animaltfdb.R
@@ -1,9 +1,12 @@
 # Import animalTFDB data
 
 library(tidyverse)
+library(magrittr)
 
+# 180423 -----------------------------------------------------------
 site <- "http://bioinfo.life.hust.edu.cn/AnimalTFDB/download_index/"
 file <- "Homo_sapiens_transcription_factors_gene_list.txt"
 url <- paste0(site,file)
-
-raw_tfdb <- read_tsv(url)
+animaltfdb.180423 <- read_tsv(url)
+animaltfdb.180423 %<>% set_tidy_names(syntactic = T)
+devtools::use_data(animaltfdb.180423)
