714628026a45a323c165729b244af37643c2cbae
mal2017 <matt.a.lawlor@gmail.com>
2018-06-11 12:00:51 -0400

    removed auto license and added citation to data man pg

--- R/data.R
+++ R/data.R
@@ -6,8 +6,11 @@
 #' 2. Chromatin Remodelers
 #' 3. Transcriptional Cofactors
 #'
-#'1. AnimalTFDB: a comprehensive animal transcription factor database. Hong-Mei Zhang, Hu Chen, Wei Liu, Hui Liu, Jing Gong, Huili Wang and An-Yuan Guo*. Nucl. Acids Res. 2012. 40(Database issue):D144-9.
-#'2. AnimalTFDB 2.0: a resource for expression, prediction and functional study of animal transcription factors. Hong-Mei Zhang$, Teng Liu$, Chun-Jie Liu, Shuangyang Song, Xiantong Zhang, Wei Liu, Haibo Jia, Yu Xue, and An-Yuan Guo*. Nucl. Acids Res. (28 January 2015) 43 (D1): D76-D81.
+#'
+#' Source:
+#' 1. AnimalTFDB: a comprehensive animal transcription factor database. Hong-Mei Zhang, Hu Chen, Wei Liu, Hui Liu, Jing Gong, Huili Wang and An-Yuan Guo*. Nucl. Acids Res. 2012. 40(Database issue):D144-9.
+#'
+#' 2. AnimalTFDB 2.0: a resource for expression, prediction and functional study of animal transcription factors. Hong-Mei Zhang$, Teng Liu$, Chun-Jie Liu, Shuangyang Song, Xiantong Zhang, Wei Liu, Haibo Jia, Yu Xue, and An-Yuan Guo*. Nucl. Acids Res. (28 January 2015) 43 (D1): D76-D81.
 #'
 #' @format tibble
 "animaltfdb.180611"
