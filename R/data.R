92b0f37899d1ae04f105162362439351011dc126
mal2017 <matt.a.lawlor@gmail.com>
2018-06-11 11:50:16 -0400

    updated data and added remods,cofacs

---
+++ R/data.R
@@ -0,0 +1,10 @@
+#' animaltfdb.180611
+#'
+#' Tibble version of three tables from the animalTFDB2.0 database.
+#'
+#' 1. Transcription Factors
+#' 2. Chromatin Remodelers
+#' 3. Transcriptional Cofactors
+#'
+#' @format tibble
+"animaltfdb.180611"
--- data-raw/pull-raw-animaltfdb.R
+++ data-raw/pull-raw-animaltfdb.R
@@ -2,11 +2,54 @@
 
 library(tidyverse)
 library(magrittr)
+library(Organism.dplyr)
 
-# 180423 -----------------------------------------------------------
 site <- "http://bioinfo.life.hust.edu.cn/AnimalTFDB/download_index/"
+src <- src_organism("TxDb.Hsapiens.UCSC.hg19.knownGene")
+
+
+# 180611 tfs ------------------------------------------------------
 file <- "Homo_sapiens_transcription_factors_gene_list.txt"
 url <- paste0(site,file)
-animaltfdb.180423 <- read_tsv(url)
-animaltfdb.180423 %<>% set_tidy_names(syntactic = T)
-devtools::use_data(animaltfdb.180423)
+animaltfdb.tfs.180611 <- read_tsv(url)
+animaltfdb.tfs.180611 %<>%
+  set_tidy_names(syntactic = T) %>%
+  rename(ensembl = "Ensembl.ID", entrez = "Entrez.ID")
+
+tfs <- tbl(src, "id") %>%
+  collect() %>%
+  left_join(animaltfdb.tfs.180611, ., by = c("ensembl", "entrez")) %>%
+  mutate(Type = "Transcription_Factors") %>%
+  dplyr::select(Type, symbol, ensembl, entrez, alias, map)
+
+# 180611 remodelers ------------------------------------------------------
+file <- "Homo_sapiens_chromatin_remodeling_factors_gene_list.txt"
+url <- paste0(site,file)
+animaltfdb.remod.180611 <- read_tsv(url)
+animaltfdb.remod.180611 %<>%
+  set_tidy_names(syntactic = T) %>%
+  rename(ensembl = "Ensembl.ID", entrez = "Entrez.ID") %>%
+  mutate(entrez = as.character(entrez))
+
+remods <- tbl(src, "id") %>%
+  collect() %>%
+  left_join(animaltfdb.remod.180611, ., by = c("ensembl", "entrez")) %>%
+  dplyr::select(Type, symbol, ensembl, entrez, alias, map)
+
+# 180611 cofactors ------------------------------------------------------
+file <- "Homo_sapiens_transcription_co-factors_gene_list.txt"
+url <- paste0(site,file)
+animaltfdb.cofac.180611 <- read_tsv(url)
+animaltfdb.cofac.180611 %<>%
+  set_tidy_names(syntactic = T) %>%
+  rename(ensembl = "Ensembl.ID", entrez = "Entrez.ID") %>%
+  mutate(entrez = as.character(entrez))
+
+cofacs <- tbl(src, "id") %>%
+  collect() %>%
+  left_join(animaltfdb.cofac.180611, ., by = c("ensembl", "entrez")) %>%
+  dplyr::select(Type, symbol, ensembl, entrez, alias, map)
+
+animaltfdb.180611 <- bind_rows(tfs, remods, cofacs)
+
+devtools::use_data(animaltfdb.180611)
